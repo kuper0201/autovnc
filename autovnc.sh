60f95a91da49ca4df7eb8cf54bcc914ea21a3024
kuper0201 <kuper171@gmail.com>
2024-01-03 15:40:13 +0900

    Update autovnc.sh

--- autovnc.sh
+++ autovnc.sh
@@ -5,15 +5,15 @@ if [ $(id -u) -ne 0 ]; then exec sudo bash "$0" "$@"; exit; fi
 
 cd /tmp
 
-# Download VNC Install Script
-wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.6.0-Linux-x64-ANY.tar.gz
+# Download VNC Install Script(Latest version of 2024/01/03)
+wget https://downloads.realvnc.com/download/file/vnc.files/VNC-Server-7.8.0-Linux-x64-ANY.tar.gz
 
 # Extract VNC Script
-mv VNC-Server-7.6.0-Linux-x64-ANY.tar.gz vnc.tar.gz
+mv VNC-Server-7.8.0-Linux-x64-ANY.tar.gz vnc.tar.gz
 tar -xvf vnc.tar.gz
 
 # Install VNC
-cd VNC-Server-7.6.0-Linux-x64
+cd VNC-Server-7.8.0-Linux-x64
 chmod +x vncinstall
 ./vncinstall
 systemctl enable vncserver-x11-serviced.service
@@ -25,7 +25,7 @@ vncpasswd -service
 
 # Cleanup
 rm -rf /tmp/vnc.tar.gz
-rm -rf /tmp/VNC-Server-7.6.0-Linux-x64
+rm -rf /tmp/VNC-Server-7.8.0-Linux-x64
 
 # Reboot
 echo "Reboot the System now"
